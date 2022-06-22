232525b0f44bff4942225b30e7d7fc6e7995e1fb
Vincent Lemstra <vincentlemstra@gmail.com>
2022-06-22 09:24:28 +0000

    Added checklist

---
+++ index.js
@@ -0,0 +1,16 @@
+// Checklist:
+// When started, it will ask you if you want to do a calculation. If you answer “Y” it will ask three questions:
+
+// What is the first number?
+
+// Which operation do you want to do?
+
+// What is the second number?
+
+// The calculator will then respond in this way: “5 + 7 = 12”.
+
+// It will then loop back to ask you if you want to do a calculation.
+
+// Remember that when the user types in, the operation it’s a string. You can compare that string to other strings like “+” or “-” to see what operation needs to be done. Don’t forget to cast numerical input to actual numbers!
+
+// You can assume the user types in the correct character for the operation. If you want to, you can check this but you don’t need to.

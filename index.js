e106f08bedbb7e20ed2e7f530b976108bad235fd
Vincent Lemstra <vincentlemstra@gmail.com>
2022-06-22 10:00:39 +0000

    Add all operation

--- index.js
+++ index.js
@@ -17,6 +17,15 @@ while (true) {
       case "+":
         calculation = firstNumber + secondNumber;
         break;
+      case "-":
+        calculation = firstNumber - secondNumber;
+        break;
+      case "*":
+        calculation = firstNumber * secondNumber;
+        break;
+      case "/":
+        calculation = firstNumber / secondNumber;
+        break;
     }
 
     console.log(`${firstNumber} ${operation} ${secondNumber} = ${calculation}`);
@@ -26,18 +35,3 @@ while (true) {
 }
 
 console.log("Oke, bye.");
-
-// Checklist:
-// When started, it will ask you if you want to do a calculation. If you answer “Y” it will ask three questions:
-
-// 1. What is the first number?
-// 2. Which operation do you want to do?
-// 3. What is the second number?
-
-// The calculator will then respond in this way: “5 + 7 = 12”.
-
-// It will then loop back to ask you if you want to do a calculation.
-
-// Remember that when the user types in, the operation it’s a string. You can compare that string to other strings like “+” or “-” to see what operation needs to be done. Don’t forget to cast numerical input to actual numbers!
-
-// You can assume the user types in the correct character for the operation. If you want to, you can check this but you don’t need to.
